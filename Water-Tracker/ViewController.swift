fc7160e61f9eaf3b13ba1b80d454a0dfbb5920d8
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2022-12-02 17:22:07 +0300

    Visual part is complete.

--- Water-Tracker/ViewController.swift
+++ Water-Tracker/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+     //MARK: -UI ELEMENTS
+    
+    
+    
+    @IBOutlet var myButton : UIButton!
+    //MARK: -PROPERTIES
 
+    
+//MARK: -LIFE CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myButton.setImage(UIImage(named: "pngwing.com-3.png"), for: .normal)
     }
+    
+    //MARK: -FUNCTIONS
+    
+    //MARK: -ACTIONS
 
 
 }
 
+    
